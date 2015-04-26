daa23fef871803d823a1b09063f181f06967f6e9
Ngeng Chhengkim <chhengkimngeng@gmail.com>
2015-04-26 21:34:18 +0700

    complete sign up

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 source 'https://rubygems.org'
+gem 'bootstrap-sass',       '3.2.0.0'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
+gem 'bcrypt',               '3.1.7'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'new''users/new'
+
+  get 'static_page/home'
+
+  get 'help' => 'static_page#help'
+  get 'about' => 'static_page#about'
+
+
+  get 'singup' => 'users#new'
+
+  root 'static_page#home'
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
